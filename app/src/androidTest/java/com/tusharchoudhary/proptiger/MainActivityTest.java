2a66c913103e8510c6a43e542806f2a234bd2a4e
Tushar Chaudhary <tusharchaudhary@PT-MAC.local>
2016-06-23 18:39:45 +0530

    something

---
+++ app/src/androidTest/java/com/tusharchoudhary/proptiger/MainActivityTest.java
@@ -0,0 +1,16 @@
+package com.tusharchoudhary.proptiger;
+
+
+import org.junit.Test;
+import static org.junit.Assert.assertEquals;
+
+/**
+ * Created by tusharchaudhary on 6/23/16.
+ */
+public class MainActivityTest {
+    @Test
+    public void addition_isCorrect() throws Exception {
+        assertEquals(4, 2 + 2);
+    }
+
+}
